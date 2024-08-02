f855e075154985ae36f875ed559163c68604c8f7
BernardMaacaron <bernard_maacaron@hotmail.com>
2024-08-02 03:15:58 +0200

    Chore: to run SCM_to_frame on results

--- utils/SCM_to_frame.py
+++ utils/SCM_to_frame.py
@@ -4,6 +4,11 @@ import os
 import numpy as np
 import sys
 import glob
+import os
+
+# Get the directory of the current script
+current_dir = os.getcwd()
+sys.path.append(current_dir)
 
 import BrianHF
 from datasets.utils.parsing import import_yarp_skeleton_data, batchIterator
@@ -42,11 +47,11 @@ def process(data_dvs_file, output_path, skip=None, args=None):
     frame_width = np.max(data_dvs['x'])+1
     frame_height = np.max(data_dvs['y'])+1
     
-    eros = EROS(kernel_size=args['eros_kernel'], frame_width=frame_width, frame_height=frame_height)
 
+    
     poses_movenet = []
     if args['write_video']:
-        output_path_video = os.path.join(output_path,'eros-out.mp4')
+        output_path_video = os.path.join(output_path,'scm-out.mp4')
         print(output_path_video)
         video_out = cv2.VideoWriter(output_path_video, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), args['fps'],
                                     (frame_width, frame_height))
@@ -54,26 +59,100 @@ def process(data_dvs_file, output_path, skip=None, args=None):
     for fi, (events, pose, batch_size) in enumerate(iterator):
         sys.stdout.write(f'frame: {fi}/{len(data_ts["ts"])}\r')
         sys.stdout.flush()
+        # old_vx = None
+        # old_vy = None
+
+        frame = np.zeros((frame_height, frame_width), dtype=np.uint8)
 
         # if args['dev']:
         #     print('frame: ', fi)
-        for ei in range(batch_size):
-            eros.update(vx=int(events['x'][ei]), vy=int(events['y'][ei]))
+        for ei in range(batch_size):                
+            vx=int(events['x'][ei])
+            vy=int(events['y'][ei])
+            
+            # if old_vx is None and old_vy is None:
+            #     old_vx = vx
+            #     old_vy = vy
+            # else:
+            #     frame[old_vx,old_vy] = 0
+            #     old_vx = vx
+            #     old_vy = vy
+                
+            frame[vy,vx] = 255
+        
+            
         if fi % skip != 0:
             continue
-
 
         # frame = cv2.GaussianBlur(frame, (args['gauss_kernel'], args['gauss_kernel']), 0)
 
         filename = os.path.basename(data_dvs_file)
+        
         if args['write_images']:
             cv2.imwrite(os.path.join(output_path, f'{filename}_{fi:04d}.jpg'), frame)
         if args['write_video']:
-            video_out.write(frame)
-            print('writing')
+            framergb = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+            video_out.write(framergb)
 
     if args['write_video']:
         video_out.release()
 
     return
 
+def main():
+    # Define the variables directly
+    eros_kernel = 8
+    # frame_width = 640
+    # frame_height = 480
+    gauss_kernel = 7
+    skip_image = None
+    input_data_dir = 'SimulationResultsFinal'
+    output_base_path = 'EROS_like'
+    write_images = False
+    write_video = True
+    frame_length = 30 #ms
+    interval_length = 1000 #ms
+    fps = interval_length/frame_length
+    dev = False
+    ts_scaler = 1.0
+    
+    # Ensure the base output path exists
+    output_base_path = os.path.abspath(output_base_path)
+    ensure_location(output_base_path)
+    input_data_dir = os.path.abspath(input_data_dir)
+    
+    datasets = ['DHP19_Sample', 'EyeTracking', 'h36m_sample', 'MVSEC_short_outdoor']
+    datasets = ['h36m_sample', 'MVSEC_short_outdoor']
+
+    for dataset in datasets:
+        input_data_dir = os.path.join(input_data_dir, dataset)
+        # Iterate over .log files in the input data directory
+        log_files = glob.glob(os.path.join(input_data_dir, '**/data.log'), recursive=True)
+        for log_file in log_files:
+            try:
+                # Create a corresponding output path
+                relative_path = os.path.relpath(log_file, input_data_dir)
+                output_path = os.path.join(output_base_path, dataset, os.path.dirname(relative_path))
+                ensure_location(output_path)
+
+                print("Processing:", log_file)
+                print("Output path:", output_path)
+
+                # Create a dictionary to hold the arguments
+                args = {
+                    'eros_kernel': eros_kernel,
+                    # 'frame_width': frame_width,
+                    # 'frame_height': frame_height,
+                    'gauss_kernel': gauss_kernel,
+                    'write_images': write_images,
+                    'write_video': write_video,
+                    'fps': fps,
+                    'dev': dev,
+                    'ts_scaler': ts_scaler
+                }
+                process(log_file, output_path, skip=skip_image, args=args)
+            except:
+                print(f"Error processing {log_file}")
+
+if __name__ == '__main__':
+    main()
